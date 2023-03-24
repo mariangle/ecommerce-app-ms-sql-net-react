171fb8375c79eae91415b66385973c809fcb9f0a
Maria <mariale171001@gmail.com>
2023-03-24 18:05:58 +0100

    initial setup

---
+++ client/src/productState.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function productState() {
+  return [
+    {
+        id: 1,
+        name: 'Product 1',
+        description: 'This is product 1',
+        price: 10.99,
+        image: 'product1.jpg'
+    }
+  ]
+}
+
+export default productState
